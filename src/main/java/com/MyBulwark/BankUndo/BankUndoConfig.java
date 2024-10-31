689b4f9912469f0c185cc8262f8720a1bbfdd3f8
Sam Benn <sam-benn@hotmail.co.uk>
2024-10-31 16:57:03 +0000

    Initial setup

--- src/main/java/com/MyBulwark/BankUndo/BankUndoConfig.java
+++ src/main/java/com/MyBulwark/BankUndo/BankUndoConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.MyBulwark.BankUndo;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
-public interface ExampleConfig extends Config
+@ConfigGroup("BankUndo")
+public interface BankUndoConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/MyBulwark/BankUndo/BankUndoTest.java
+++ src/test/java/com/MyBulwark/BankUndo/BankUndoTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.MyBulwark.BankUndo;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class BankUndoTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(BankUndoPlugin.class);
 		RuneLite.main(args);
 	}
 }
